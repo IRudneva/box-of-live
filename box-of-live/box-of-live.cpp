588643b422d35f386b633aada0db2c200fc0ce96
IRudneva <stolya2707@yandex.ru>
2023-02-17 18:03:33 +0200

    added playing field and config (in progress)

--- box-of-live/box-of-live.cpp
+++ box-of-live/box-of-live.cpp
@@ -1,6 +1,31 @@
 ﻿#include <TGUI/TGUI.hpp>
 #include <SFML/Graphics.hpp>
 
+struct GraphicConfig
+{
+	struct Settings {
+		tgui::Color color_layout = tgui::Color::White;
+		tgui::Layout height_layout = "200";
+
+		std::string text_start_button = "START";
+		tgui::Color color_start_button_background = tgui::Color::Green;
+		tgui::Color color_start_button_background_down = tgui::Color::Red;
+		tgui::Color color_start_button_background_hover = tgui::Color::Magenta;
+		tgui::Layout2d size_start_button = { 150,70 };
+		tgui::Layout2d pos_start_button = { 500, 55 };
+
+		int count_labels = 3;
+		std::vector<std::string> text_labels = {"Name_1", "Name_2", "Name_3"};
+		std::vector<tgui::Layout2d> pos_labels = { {50, 30},{50, 60}, {50, 90} };
+		size_t text_size = 14;
+	} settings;
+
+	struct GameField {
+		tgui::Color color_layout = tgui::Color::White;
+		tgui::Layout height_layout = "400";
+	} game_field;
+};
+
 class GraphicScene
 {
 public:
@@ -8,38 +33,38 @@ public:
 		: gui_(window)
 	{}
 
-	void init()
+	void init(const GraphicConfig& config)
 	{
-		tgui::Group::Ptr fields = tgui::Group::create();
+		auto fields = tgui::Group::create();
 
-		tgui::Panel::Ptr base_layer = tgui::Panel::create();
-		base_layer->getRenderer()->setBackgroundColor(tgui::Color::Black);
-
-		tgui::Panel::Ptr settings_layout = tgui::Panel::create();
-		settings_layout->getRenderer()->setBackgroundColor(tgui::Color::White);
+		auto settings_layout = tgui::Panel::create();
+		settings_layout->getRenderer()->setBackgroundColor(config.settings.color_layout);
 		settings_layout->setPosition(tgui::bindLeft(fields), tgui::bindTop(fields));
-		settings_layout->setHeight("200");
+		settings_layout->setHeight(config.settings.height_layout);
 
-		tgui::Panel::Ptr game_layout = tgui::Panel::create();
-		game_layout->getRenderer()->setBackgroundColor(tgui::Color::White);
-		game_layout->setPosition(tgui::bindLeft(settings_layout), tgui::bindBottom(settings_layout) + 5);
-		game_layout->setHeight("395");
+		auto game_layout = tgui::Panel::create();
+		game_layout->getRenderer()->setBackgroundColor(config.game_field.color_layout);
+		game_layout->setPosition(tgui::bindLeft(settings_layout), tgui::bindBottom(settings_layout) );
+		game_layout->setHeight(config.game_field.height_layout);
 
-		fields->add(base_layer);
+		auto canvas = tgui::CanvasSFML::create();
+		game_layout->add(canvas, "my_canvas");
+		canvas_ = canvas;
+
 		fields->add(settings_layout);
 		fields->add(game_layout);
 
 		gui_.add(fields);
 
-		tgui::Group::Ptr buttons = tgui::Group::create();
+		auto buttons = tgui::Group::create();
 
-		tgui::Button::Ptr start_button = tgui::Button::create();
-		start_button->getRenderer()->setBackgroundColor(tgui::Color::Yellow);
-		start_button->getRenderer()->setBackgroundColorDown(tgui::Color::Blue);
-		start_button->getRenderer()->setBackgroundColorHover(tgui::Color::Green);
-		start_button->setSize(150, 70);
-		start_button->setPosition(tgui::bindLeft(settings_layout) + 600, tgui::bindTop(settings_layout) + 100);
-		start_button->setText("START");
+		auto start_button = tgui::Button::create();
+		start_button->getRenderer()->setBackgroundColor(config.settings.color_start_button_background);
+		start_button->getRenderer()->setBackgroundColorDown(config.settings.color_start_button_background_down);
+		start_button->getRenderer()->setBackgroundColorHover(config.settings.color_start_button_background_hover);
+		start_button->setSize(config.settings.size_start_button);
+		start_button->setPosition(config.settings.pos_start_button);
+		start_button->setText(config.settings.text_start_button);
 		//	start_button->onPress([=] { settings_layout->setVisible(false); });
 
 		buttons->add(start_button);
@@ -47,28 +72,18 @@ public:
 		gui_.add(buttons);
 
 		tgui::Group::Ptr labels = tgui::Group::create();
-
-		tgui::Label::Ptr label1 = tgui::Label::create();
-		label1->setText("Name1:");
-		label1->setPosition(tgui::bindLeft(settings_layout) + 50, tgui::bindTop(settings_layout) + 30);
-		label1->setTextSize(14);
-		labels->add(label1);
-
-		tgui::Label::Ptr label2 = tgui::Label::create();
-		label2->setText("Name2:");
-		label2->setPosition(tgui::bindLeft(settings_layout) + 50, tgui::bindBottom(label1) + 30);
-		label2->setTextSize(14);
-		labels->add(label2);
-
-		tgui::Label::Ptr label3 = tgui::Label::create();
-		label3->setText("Name3:");
-		label3->setPosition(tgui::bindLeft(settings_layout) + 50, tgui::bindBottom(label2) + 30);
-		label3->setTextSize(14);
-		labels->add(label3);
-
+	
+		for(int i = 0; i < config.settings.count_labels; i++)
+		{
+			auto label = tgui::Label::create();
+			label->setText(config.settings.text_labels[i]);
+			label->setTextSize(config.settings.text_size);
+			label->setPosition(config.settings.pos_labels[i]);
+			labels->add(label);
+		}
 		gui_.add(labels);
 
-		tgui::Group::Ptr e_box = tgui::Group::create();
+		/*tgui::Group::Ptr e_box = tgui::Group::create();
 
 		auto editBox1 = tgui::EditBox::create();
 		editBox1->setSize(100, 16);
@@ -94,7 +109,7 @@ public:
 
 		e_box->add(editBox3);
 
-		gui_.add(e_box);
+		gui_.add(e_box);*/
 	}
 
 	void handleEvent(const sf::Event & event)
@@ -106,19 +121,39 @@ public:
 	{
 		gui_.draw();
 		
-	/*	sf::RectangleShape rect;
-		rect.setFillColor(sf::Color::Black);
-		rect.setOutlineColor(sf::Color::Magenta);
-		rect.setPosition(40, 40);
-		rect.setSize(sf::Vector2f(50, 50));
-		if (auto canvas = myCanvas_.lock(); canvas != nullptr) {
-			canvas->clear(tgui::Color::Cyan);
-			canvas->draw(rect);
+		sf::RectangleShape rect;
+		rect.setFillColor(sf::Color::White);
+		rect.setOutlineThickness(1.0f);
+		rect.setOutlineColor(sf::Color::Black);
+		rect.setPosition(0, 0);
+		rect.setSize(sf::Vector2f(10, 10));
+		
+		std::vector<sf::RectangleShape> rects1(80, rect);
+		std::vector<sf::RectangleShape> rects2(40, rect);
+
+		sf::Vector2f pos(0,0);
+
+		if (auto canvas = canvas_.lock(); canvas != nullptr) {
+			canvas->clear(tgui::Color::Green);
+
+			for (auto i = 0; i < rects2.size(); i++)
+			{
+				rects2[i].setPosition(pos.x, pos.y);
+				canvas->draw(rects2[i]);
+				for (auto j = 0; j < rects1.size(); j++)
+				{
+					rects1[j].setPosition(pos.x+10, pos.y);
+					pos = rects1[j].getPosition();
+					canvas->draw(rects1[j]);
+				}
+				
+				pos = sf::Vector2f(rects2[i].getPosition().x, rects2[i].getPosition().y+10);
+			}
 			canvas->display();
-		}*/
+		}
 	}
 private:
-	//std::weak_ptr<tgui::CanvasSFML> myCanvas_;
+	std::weak_ptr<tgui::CanvasSFML> canvas_;
 	tgui::GuiSFML gui_;
 };
 
@@ -144,7 +179,8 @@ void mainLoop(sf::RenderWindow& window, GraphicScene& scene)
 int main()
 {
 	sf::RenderWindow window{ {800, 600}, "Box of Live" };
-	GraphicScene scene{ window };
-	scene.init();
+	GraphicScene scene(window);
+	GraphicConfig gc;
+	scene.init(gc);
 	mainLoop(window, scene);
 }
